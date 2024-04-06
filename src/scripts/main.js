45fbe0d22e7cb132633a90acfadb335396b2defa
Russelkun <150195769+Russelkun@users.noreply.github.com>
2024-04-06 23:15:11 +0200

    --added contact form

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,3 +1,35 @@
 //
 
+const { header } = require("express/lib/request");
+
 console.log("I am connected");
+
+const openNav = document.getElementById("burger-nav");
+const navContent = document.querySelector("nav");
+
+const accordionHeader = document.querySelectorAll(".accordion__header");
+
+openNav.addEventListener("click", () => {
+  console.log("Angela, I'm pregnant");
+  if (navContent.style.maxHeight === "0") {
+    return (navContent.style.maxHeight = "100%");
+  } else {
+    return (navContent.style.maxHeight = "100%");
+  }
+});
+
+accordionHeader.forEach((header) => {
+  header.addEventListener("click", () => {
+    console.log("moans in japanese!");
+    const content = header.nextElementSibling;
+    const isOpen = header.getAttribute("aria-expanded") === "true" || false;
+
+    if (isOpen) {
+      header.setAttribute("aria-expanded", "false");
+      content.style.display = "none";
+    } else {
+      header.setAttribute("aria-expanded", "true");
+      content.style.display = "block";
+    }
+  });
+});
